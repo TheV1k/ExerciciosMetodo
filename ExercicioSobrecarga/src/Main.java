e2cdc066db54a2aa9f0e64ad9f3a9ed3b5e064a2
TheV1k <62910266+TheV1k@users.noreply.github.com>
2023-05-09 07:40:52 -0300

    Add files via upload

---
+++ ExercicioSobrecarga/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Exercício quadrilátero
+        System.out.println("Exercicio Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+          }
+}
